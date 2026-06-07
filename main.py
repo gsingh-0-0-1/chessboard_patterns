bb09ee19ca99f96ed97684caf0c758b843809d90
gsingh-0-0-1 <gurmehar@gmail.com>
2026-06-06 21:50:38 -0700

    some updates, new image

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ import json
 from PIL import Image
 
 
-def coords_to_spiral_index(x, y):
+
+def coords_to_square_spiral_index(x, y):
 	radius = max(abs(x), abs(y))
 	if radius == 0: return 0
 	starting_perfect_square_root = (radius * 2 - 1)
@@ -30,7 +31,7 @@ def coords_to_spiral_index(x, y):
 	return spiral_index
 
 
-def spiral_index_to_coords(spiral_index):
+def square_spiral_index_to_coords(spiral_index):
 	# compute coordinates based on square number
 	radius = math.floor(math.sqrt(spiral_index))
 	if radius % 2 == 0:
@@ -72,16 +73,6 @@ def board_square_dict(attacked_by: set = set(), played_by: int = -1):
 	}
 
 DEFAULT_BOARD_SQUARE_DICT = {'attacked_by' : set(), 'played_by' : -1}
-DEFAULT_REL_ATTACK_POS = [
-	[-2, -1],
-	[-2, 1],
-	[-1, -2],
-	[-1, 2],
-	[1, -2],
-	[1, 2],
-	[2, -1],
-	[2, 1]
-]
 
 class Board:
 	def __init__(self, config):
@@ -101,11 +92,11 @@ class Board:
 
 		print("Board initialized.")
 
-	def get_attacked_positions(self, n):
-		coords = spiral_index_to_coords(n)
+	def get_attacked_positions(self, n, player_ind):
+		coords = square_spiral_index_to_coords(n)
 		positions = []
 
-		for pos in self.relative_attack_positions:
+		for pos in self.relative_attack_positions[player_ind]:
 			positions.append((coords[0] + pos[0], coords[1] + pos[1]))
 		return positions
 
@@ -136,59 +127,67 @@ class Board:
 			return
 
 		self.board[self.lowest_playables[player_ind]]['played_by'] = player_ind
-		arrpos = spiral_index_to_coords(self.lowest_playables[player_ind])
+		arrpos = square_spiral_index_to_coords(self.lowest_playables[player_ind])
 		
 		self.arr[arrpos[1] + self.radius, arrpos[0] + self.radius] = self.colors[player_ind]
 
-		for pos in self.get_attacked_positions(self.lowest_playables[player_ind]):
-			self.attack_position(coords_to_spiral_index(pos[0], pos[1]), player_ind)
+		for pos in self.get_attacked_positions(self.lowest_playables[player_ind], player_ind):
+			self.attack_position(coords_to_square_spiral_index(pos[0], pos[1]), player_ind)
 
+def circle_coords(r):
+	return list(set([(int(r * math.cos(theta)), int(r * math.sin(theta))) for theta in np.arange(0, 2 * math.pi, 0.01)]))
+
+def knight_coords(l_len = 1):
+	l = []
+
+	for xm in [-l_len, l_len]:
+		for ym in [-l_len, l_len]:
+			l.append([2 * xm, 1 * ym])
+			l.append([1 * xm, 2 * ym])
+
+	return l
 
 def main():
 	CONFIGS = [
-{
-	"radius" : 1000,
-	"nplayers" : 2,
-	"relative_attack_positions" : [
-		[1, 2],
-		[-5, 1],
-		[0, -4],
-		[-1, -2],
-		[5, -1],
-		[-0, -4],
-	]
-}
+		{
+		"radius": 1000,
+		"nplayers": 4,
+		"relative_attack_positions": [
+			knight_coords() + knight_coords(2) + knight_coords(4) + knight_coords(8),
+			knight_coords() + knight_coords(2) + knight_coords(4),
+			knight_coords() + knight_coords(2),
+			knight_coords()
+		]
+		}
 	]
 
 	for config in CONFIGS:
-		for nplayers in range(1, 12):
-			config["nplayers"] = nplayers
-			board = Board(config = config)
+		board = Board(config = config)
 
-			while True:
-				try:
-					for player in range(board.nplayers):
-						board.play(player)
-						# plt.imshow(board.arr[::-1])
-						# plt.pause(0.1)
-						# plt.clf()
-				except Exception as e:
-					print(e)
-					break
+		while True:
+			try:
+				for player in range(board.nplayers):
+					board.play(player)
+					# plt.imshow(board.arr[::-1])
+					# plt.pause(1)
+					# plt.clf()
+			except Exception as e:
+				print(e)
+				break
 
-			t = int(time.time() * 1000)
-			os.makedirs(f'outputs/{t}')
-			with open(f'outputs/{t}/config.json', 'w') as f:
-				json.dump(board.config, f)
-			# plt.figure(figsize = (8, 8))
-			# plt.imshow(board.arr[::-1])
-			#plt.savefig(f'outputs/{t}/board.png')
+		t = int(time.time() * 1000)
+		os.makedirs(f'outputs/{t}')
+		with open(f'outputs/{t}/config.json', 'w') as f:
+			json.dump(board.config, f)
+		# plt.figure(figsize = (8, 8))
+		# plt.imshow(board.arr[::-1])
+		#plt.savefig(f'outputs/{t}/board.png')
 
-			array = (board.arr[::-1] * 255).astype(np.uint8)
-			img = Image.fromarray(array)
-			img.save(f'outputs/{t}/board.png')
+		array = (board.arr[::-1] * 255).astype(np.uint8)
+		img = Image.fromarray(array)
+		img.save(f'outputs/{t}/board.png')
 
-			# plt.imshow(board.arr)
-			# plt.show()
+		# plt.imshow(board.arr)
+		# plt.show()
 
 main()
